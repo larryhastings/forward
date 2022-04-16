b43b7b7530a268c679bae3058c71a054973bc4a2
Larry Hastings <larry@hastings.org>
2022-04-15 18:57:33 -0700

    Minor tweak to magic_method_tester.py.
    Now instead of hard-coding the steps in order they're run,
    I added a function that spits out a monotonically increasing
    number.

--- experiments/magic_method_tester.py
+++ experiments/magic_method_tester.py
@@ -1,3 +1,10 @@
+_step_no = 0
+
+def step_no():
+    global _step_no
+    _step_no += 1
+    return _step_no
+
 class MyPreparedDict(dict):
     def __repr__(self):
         super_repr = dict.__repr__(self)
@@ -8,30 +15,30 @@ class MyPreparedDict(dict):
 
 class MetaClass_Base(type):
     def __init_subclass__(cls, **kwargs):
-        print(f"step 0: metaclass_base.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: metaclass_base.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
         return None
 
 class MetaClass(MetaClass_Base, pepsi="and milk"):
     def __prepare__(name, bases, **kwargs):
-        print(f"step 1: metaclass __prepare__\n  {name=}\n  {bases=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: metaclass __prepare__\n  {name=}\n  {bases=}\n  {kwargs=}\n")
         return MyPreparedDict()
 
     def __new__(mcls, name, bases, namespace, **kwargs):
-        print(f"step 2: metaclass __new__\n  {mcls=}\n  {name=}\n  {bases=}\n  {namespace=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: metaclass __new__\n  {mcls=}\n  {name=}\n  {bases=}\n  {namespace=}\n  {kwargs=}\n")
         return super().__new__(mcls, name, bases, namespace, **kwargs)
 
     def __init__(mcls, name, bases, namespace, **kwargs):
-        print(f"step 5: metaclass __init__\n  {mcls=}\n  {name=}\n  {bases=}\n  {namespace=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: metaclass __init__\n  {mcls=}\n  {name=}\n  {bases=}\n  {namespace=}\n  {kwargs=}\n")
         return super().__init__(name, bases, namespace, **kwargs)
 
 class BaseClass_A:
     def __init_subclass__(cls, **kwargs):
-        print(f"step 3: first baseclass.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: first baseclass.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
         return super().__init_subclass__(**kwargs)
 
 class BaseClass_B:
     def __init_subclass__(cls, **kwargs):
-        print(f"step 4: second baseclass.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
+        print(f"step {step_no()}: second baseclass.__init_subclass__\n  {cls=}\n  {kwargs=}\n")
         return None
 
 print("next up: class Foo\n")
